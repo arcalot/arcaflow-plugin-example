e2c4fdb4f1eca793c4c7db63f7e0b5d2d92b167a
mleader <mleader@redhat.com>
2023-04-14 13:35:00 -0400

    simplify hello world plugin

--- arcaflow_plugin_example/example_plugin.py
+++ arcaflow_plugin_example/example_plugin.py
@@ -1,127 +1,28 @@
 #!/usr/bin/env python3
-import re
+
 import sys
 import typing
 from dataclasses import dataclass
-
-from arcaflow_plugin_sdk import annotations, plugin, schema, validation
-
-
-@dataclass
-class FullName:
-    """A full name holds the first and last name of an individual."""
-
-    first_name: typing.Annotated[
-        str,
-        validation.min(1),
-        validation.pattern(re.compile("^[a-zA-Z]+$")),
-        schema.example("Arca"),
-        schema.name("First name"),
-    ]
-    last_name: typing.Annotated[
-        str,
-        validation.min(1),
-        validation.pattern(re.compile("^[a-zA-Z]+$")),
-        schema.example("Lot"),
-        schema.name("Last name"),
-    ]
-
-    def __str__(self) -> str:
-        """
-        :return: the string representation of this name
-        """
-        return self.first_name + " " + self.last_name
-
-
-@dataclass
-class Nickname:
-    """A nickname is a simplified form of the name that only holds the
-    preferred name of an individual."""
-
-    nick: typing.Annotated[
-        str,
-        validation.min(1),
-        validation.pattern(re.compile("^[a-zA-Z]+$")),
-        schema.example("Arcalot"),
-        schema.name("Nickname"),
-    ]
-
-    def __str__(self) -> str:
-        """
-        :return: the string representation of this name
-        """
-        return self.nick
+from arcaflow_plugin_sdk import plugin, validation
 
 
 @dataclass
 class InputParams:
-    """This is the data structure for the input parameters of the step defined
-    below."""
-
-    name: typing.Annotated[
-        typing.Union[
-            typing.Annotated[
-                FullName,
-                annotations.discriminator_value("fullname"),
-                schema.name("Full name"),
-            ],
-            typing.Annotated[
-                Nickname,
-                annotations.discriminator_value("nickname"),
-                schema.name("Nick"),
-            ],
-        ],
-        schema.name("Name"),
-        schema.description("Who do we say hello to?"),
-        annotations.discriminator("_type"),
-        schema.example(
-            {
-                "_type": "fullname",
-                "first_name": "Arca",
-                "last_name": "Lot",
-            }
-        ),
-        schema.example(
-            {
-                "_type": "nickname",
-                "nick": "Arcalot",
-            }
-        ),
-    ]
+    name: typing.Annotated[str, validation.min(1)]
 
 
 @dataclass
 class SuccessOutput:
-    """This is the output data structure for the success case."""
-
     message: str
-
-
-@dataclass
-class ErrorOutput:
-    """This is the output data structure in the error  case."""
-
-    error: str
 
 
 @plugin.step(
     id="hello-world",
     name="Hello world!",
     description="Says hello :)",
-    outputs={"success": SuccessOutput, "error": ErrorOutput},
+    outputs={"success": SuccessOutput},
 )
-def hello_world(
-    params: InputParams,
-) -> typing.Tuple[str, typing.Union[SuccessOutput, ErrorOutput]]:
-    """The function  is the implementation for the step. It needs the decorator
-    above to make it into a  step. The type hints for the params are required.
-
-    :param params:
-
-    :return: the string identifying which output it is, as well the output
-        structure
-    """
-
+def hello_world(params: InputParams) -> typing.Tuple[str, SuccessOutput]:
     return "success", SuccessOutput("Hello, {}!".format(params.name))
 
 
@@ -129,7 +30,6 @@ if __name__ == "__main__":
     sys.exit(
         plugin.run(
             plugin.build_schema(
-                # List your step functions here:
                 hello_world,
             )
         )
--- tests/test_example_plugin.py
+++ tests/test_example_plugin.py
@@ -1,35 +1,26 @@
 #!/usr/bin/env python3
 import unittest
-
-import arcaflow_plugin_example.example_plugin as example_plugin
+from arcaflow_plugin_example import example_plugin
 from arcaflow_plugin_sdk import plugin
 
 
-class ExamplePluginTest(unittest.TestCase):
+class HelloWorldTest(unittest.TestCase):
     @staticmethod
     def test_serialization():
         plugin.test_object_serialization(
-            example_plugin.InputParams(
-                name=example_plugin.FullName("Arca", "Lot")
-            )
+            example_plugin.InputParams("Arca Lot")
         )
 
         plugin.test_object_serialization(
-            example_plugin.SuccessOutput(message="Hello, Arca Lot!")
-        )
-
-        plugin.test_object_serialization(
-            example_plugin.ErrorOutput(error="This is an error")
+            example_plugin.SuccessOutput("Hello, world!")
         )
 
     def test_functional(self):
-        input = example_plugin.InputParams(
-            name=example_plugin.FullName("Arca", "Lot")
-        )
+        input = example_plugin.InputParams(name="Arca Lot")
 
         output_id, output_data = example_plugin.hello_world(input)
 
-        # The example plugin always returns an error:
+        # The example plugin always returns a success:
         self.assertEqual("success", output_id)
         self.assertEqual(
             output_data, example_plugin.SuccessOutput("Hello, Arca Lot!")
