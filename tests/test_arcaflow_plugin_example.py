9951d5918f1015a4ae0b0c94bba33863f5a0079f
Dustin Black <dblack@redhat.com>
2023-07-11 16:38:40 +0200

    update to latest template

--- tests/test_arcaflow_plugin_example.py
+++ tests/test_arcaflow_plugin_example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 
-import arcaflow_plugin_example.example_plugin as example_plugin
+import example_plugin
 from arcaflow_plugin_sdk import plugin
 
 
@@ -9,9 +9,7 @@ class ExamplePluginTest(unittest.TestCase):
     @staticmethod
     def test_serialization():
         plugin.test_object_serialization(
-            example_plugin.InputParams(
-                name=example_plugin.FullName("Arca", "Lot")
-            )
+            example_plugin.InputParams(name=example_plugin.FullName("Arca", "Lot"))
         )
 
         plugin.test_object_serialization(
@@ -23,17 +21,13 @@ class ExamplePluginTest(unittest.TestCase):
         )
 
     def test_functional(self):
-        input = example_plugin.InputParams(
-            name=example_plugin.FullName("Arca", "Lot")
-        )
+        input = example_plugin.InputParams(name=example_plugin.FullName("Arca", "Lot"))
 
         output_id, output_data = example_plugin.hello_world(input)
 
         # The example plugin always returns an error:
         self.assertEqual("success", output_id)
-        self.assertEqual(
-            output_data, example_plugin.SuccessOutput("Hello, Arca Lot!")
-        )
+        self.assertEqual(output_data, example_plugin.SuccessOutput("Hello, Arca Lot!"))
 
 
 if __name__ == "__main__":
